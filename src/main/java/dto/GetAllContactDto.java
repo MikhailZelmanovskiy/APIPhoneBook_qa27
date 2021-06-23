846babacddefbf8ae49ca46772450dea47b10836
MikhailZelmanovskiy <strayn.mike@gmail.com>
2021-06-23 21:06:43 +0300

    Initial commit

---
+++ src/main/java/dto/GetAllContactDto.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+public class GetAllContactDto {
+    List<ContactDto> contacts;
+}
